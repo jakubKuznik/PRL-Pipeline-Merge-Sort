ccda52312d10f3abe5e74055e26599ef8aba976b
jakubKuznik <kuznja16@gmail.com>
2024-03-23 23:30:48 +0100

    Prototype

--- pms.cpp
+++ pms.cpp
@@ -5,11 +5,12 @@
 // Login: xkuzni04
 // Year: 2024
 
-/* Pipline Merge sort process scheme, where each process has 
+/* 
+Pipline Merge sort process scheme, where each process has 
 input and output fronts 
-         ------   -----  ...  ------
----- P1        P2        ...      Pn -----
-         ------   -----  ...  ------
+          ------    -----  ...  ------
+---- |P1|        |P2|      ...      |Pn| -----
+          ------    -----  ...  ------
 */
 
 #include <iostream>
@@ -23,8 +24,6 @@ input and output fronts
 
 using namespace std;
 
-
-
 /**
  * This is the first process that will read the 
  * random numbers from the file FILENUMS. The process 
@@ -34,10 +33,10 @@ void first_proces(){
     
     // send Buffer that will be used to send number to the p2 
     uint8_t sBuff;
-    // input queue 
-    std::queue<uint8_t>* q1 = new std::queue<uint8_t>();
+    
     // get my rank 
     int rank;
+    
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     
     std::cout << "I am FIRST " << std::endl;
@@ -51,44 +50,116 @@ void first_proces(){
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
     
-    // parse the input numbers into the queue 
+    // parse the input numbers and send then to the second process 
     char c;
-    while (file.get(c)) {
-        uint8_t val = static_cast<uint8_t>(c);
-        q1->push(val);
-        cout << static_cast<int>(val) << endl;
+    for (uint8_t i = 0; file.get(c); i++){
+        sBuff = static_cast<uint8_t>(c);
+        if (i % 2 == 0){
+            MPI_Send(&sBuff, 1, MPI_BYTE, (rank+1), UP, MPI_COMM_WORLD);
+        }
+        else{
+            MPI_Send(&sBuff, 1, MPI_BYTE, (rank+1), DOWN, MPI_COMM_WORLD);
+        }
     }
 
     file.close();
-
-    // p1 brain while there is number send it to the p2 
-    while (){
-        // tag 1 q1 
-        // tag 2 q2 
-        MPI_Send(^)
-    }
-
+    
     // after everything sent speciall signal to shut down everyone
 
-    /**
-    MPI_Send(&sBuff, 1, MPI_BYTE, (rank+1), UP, MPI_COMM_WORLD);
-    MPI_Send(&sBuff, 1, MPI_BYTE, (rank+1), DOWN, MPI_COMM_WORLD);
-    
-    MPI_Status status;
-    MPI_Recv(&rcBuff, 1, MPI_BYTE, rank, UP, MPI_COMM_WORLD, &status);
-    */
 
-    delete q1;
     return;    
 }
 
 void nth_proces() {
     
+    std::queue<uint8_t> q_up; 
+    std::queue<uint8_t> q_down; 
+
+    uint8_t rcBuff; 
+    uint8_t sBuff; 
+
+    int up_taken     = 0;
+    int down_taken   = 0; 
+
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    
+    // Condition for the UPPER front to start the process 
+    uint8_t condition  = 2^(rank-1);
+    bool condition_met = false;
+    
+    MPI_Status status;
 
     cout << "I am " << rank << endl;
 
+    for (uint8_t i = 0; condition_met; i++){
+        if (i % 2 == 0){
+            MPI_Recv(&rcBuff, 1, MPI_BYTE, rank, UP, MPI_COMM_WORLD, &status);
+            q_up.push(rcBuff);
+        }
+        else{
+            MPI_Recv(&rcBuff, 1, MPI_BYTE, rank, DOWN, MPI_COMM_WORLD, &status);
+            q_down.push(rcBuff);
+        }
+
+        if (!q_down.empty() && (q_up.front() == condition)){
+            condition_met = true; 
+        }
+    }
+
+    int count = 0;
+    for (uint8_t i = 0; true; i++){
+        // SEND 
+        if (count < condition){
+            // choose the bigger and remeber which front it was from 
+            if (q_up.front() > q_down.front()){
+                up_taken++;
+                sBuff = q_up.front();
+                q_up.pop();
+            }
+            else{
+                down_taken++;
+                sBuff = q_down.front();
+                q_down.pop();
+            }
+            MPI_Send(&sBuff, 1, MPI_BYTE, (rank+1), UP, MPI_COMM_WORLD);
+
+        }
+        else if (count < (condition * 2)){
+            if ((up_taken == down_taken) || (up_taken < down_taken)){
+                sBuff = q_up.front();
+                q_up.pop();
+                up_taken++;
+            }
+            else {
+                sBuff = q_down.front();
+                q_down.pop();
+                down_taken++;
+            }
+            
+            MPI_Send(&sBuff, 1, MPI_BYTE, (rank+1), DOWN, MPI_COMM_WORLD);
+        }
+        else {
+            up_taken    = 0;
+            down_taken  = 0;
+            count = 0;
+            continue;
+        } 
+
+        // RECIEVE 
+        if (i % 2 == 0){
+            MPI_Recv(&rcBuff, 1, MPI_BYTE, rank, UP, MPI_COMM_WORLD, &status);
+            q_up.push(rcBuff);
+        }
+        else {
+            MPI_Recv(&rcBuff, 1, MPI_BYTE, rank, DOWN, MPI_COMM_WORLD, &status);
+            q_down.push(rcBuff);
+        }
+        count++;
+
+    }
+
+    
 }
 
 void last_proces() {
