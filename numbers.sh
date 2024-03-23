3a7704f22e467c2a75648e985fd2bedce2b6e046
jakubKuznik <kuznja16@gmail.com>
2024-03-23 21:09:42 +0100

    Init done

--- numbers.sh
+++ numbers.sh
@@ -14,11 +14,11 @@ proc=$(python3 -c "from math import ceil; print (ceil($calc))")
 #proc=$(echo "(l($numbers)/l(2))+1" | bc -l | xargs printf "%1.0f") 
 
 # Compilation 
-#mpic++ --prefix /usr/local/share/OpenMPI -o pms pms.cpp
+mpic++ --prefix /usr/local/share/OpenMPI -o pms pms.cpp
 
 # Generate file with random nums  
 dd if=/dev/random bs=1 count=$numbers of=numbers 2> /dev/null	 
 
-#mpirun --prefix /usr/local/share/OpenMPI  -np $proc pms 				
+mpirun --prefix /usr/local/share/OpenMPI -np $proc pms 				
 
 #rm -f pms numbers					
--- pms.cpp
+++ pms.cpp
@@ -1,20 +1,91 @@
-#include <mpi.h>
+
 #include <iostream>
+#include <fstream>
+#include <queue>
+#include "mpi.h"
 
-int main(int argc, char *argv[]) {\
+#define FILENUMS "numbers"
 
+using namespace std;
+
+
+/* Pipline Merge sort process scheme, where each process has 
+input and output fronts 
+            ------   ---    ...   ---
+-------- P1        P2       ...    Pn --------- 
+            ------   ---    ...   ---
+*/
+
+/**
+ * @brief This is the first process that will read the 
+ * random numbers from the file FILENUMS. The process 
+ * will then put the value alternately to two output fronts 
+ */
+void first_proces(){
+    
+    std::queue<uint8_t>* first_queue = new std::queue<uint8_t>();
+
+    std::cout << "I am FIRST " << std::endl;
+
+    // Open the file     
+    std::ifstream file;
+    file.open(FILENUMS);
+
+    if (!file.is_open()) {
+        std::cerr << "Error opening file numbers" << std::endl;
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
+    char c;
+    while (file.get(c)) {
+        uint8_t val = static_cast<uint8_t>(c);
+        cout << static_cast<int>(val) << endl;
+    }
+    
+}
+
+void nth_proces() {
+    
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    cout << "I am " << rank << endl;
+
+}
+
+void last_proces() {
+    cout << "I am LAST" << endl;
+
+}
+
+
+int main(int argc, char *argv[]) {
 
     int rank, size;
-    // MPI::Init(&argc, &argv);
+    
+    MPI_Init(&argc, &argv);
 
-    // MPI::COMM_WORLD.Set_errhandler(MPI::ERRORS_ARE_FATAL);
-    // rank = MPI::COMM_WORLD.Get_rank();
-    // size = MPI::COMM_WORLD.Get_size();
+    // get the number of process 
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    std::cout << "I am" << rank << "of" << size << std::endl;
+    // get current procces id 
+    // MPI_COMM_WORLD - prediefined constant to match all the processes  
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    // MPI::Finalize();
+    if (rank == 1){
+        first_proces();
+    }
+    else if (rank == (size-1)){
+        last_proces();
+    }
+    else {
+        nth_proces();
+    }
 
+    cout << "I am " << rank << " of " << size << endl;
+
+    MPI_Finalize();
 
     return 0;
 }
+
